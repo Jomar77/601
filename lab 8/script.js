8842746cab9ea03be4b03b76c8b26fc5921b6310
JOMAR NACORDA <jnacorda04@gmail.com>
2023-10-18 11:58:05 +1300

    e

--- lab 8/script.js
+++ lab 8/script.js
@@ -1,12 +1,7 @@
 "use strict"
 
 function toggleMenu() {
-    let menuBox = document.getElementsByClassName('options')[0];
-    if (menuBox.style.display == "block") { 
-        menuBox.style.display = "none";
-    }
-    else { 
-        menuBox.style.display = "block";
-    }
+    let menuBox = document.getElementById('menubox');
+    menuBox.classList.toggle('active');
 }
 
