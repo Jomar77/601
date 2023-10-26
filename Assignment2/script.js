cf83029affd34c01241b5399fa76b30819e200da
JOMAR NACORDA <jnacorda04@gmail.com>
2023-10-26 16:47:55 +1300

    added javascript

--- Assignment2/script.js
+++ Assignment2/script.js
@@ -3,8 +3,8 @@
 // create a function that when is clicked, it disables the div
 
 function toggleMenu() {
-    let menuBox1 = document.getElementById('bottom-frame-15');
-    menuBox1.classList.toggle('active');
-    let menuBox2 = document.getElementById('top-frame-15');
+    let menuBox1 = document.getElementById('top-frame-15');
+    menuBox1.classList.toggle('hide');
+    let menuBox2 = document.getElementById('bottom-frame-15');
     menuBox2.classList.toggle('hide');
 }
