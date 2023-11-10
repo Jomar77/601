8ee594385aa9239a8d29d2b223a9ed2b62931ea1
JOMAR NACORDA <jnacorda04@gmail.com>
2023-11-11 00:29:57 +1300

    Refactor navigation menu and add console log statement

--- Assignment2/script.js
+++ Assignment2/script.js
@@ -3,6 +3,7 @@
 // create a function that when is clicked, it disables the div
 
 function toggleMenu() {
+    console.log("toggleMenu");
     let menuBox1 = document.getElementById('top-frame-15');
     menuBox1.classList.toggle('hide');
     let menuBox2 = document.getElementById('bottom-frame-15');
@@ -13,7 +14,7 @@ function Gotop() {
     window.scrollTo({top:0,behavior:'smooth'});
 }
 
-function toggleMenu() {
+function toggleburger() {
     let menuBox = document.getElementById('menubox');
     menuBox.classList.toggle('active');
 }
