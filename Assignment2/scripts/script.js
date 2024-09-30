a6a02650a4f965fb4a2e3f0c7ba01da40565de50
JOMAR NACORDA <jnacorda04@gmail.com>
2024-09-30 14:27:39 +1300

    Refactor npmignore files

--- Assignment2/scripts/script.js
+++ Assignment2/scripts/script.js
@@ -18,3 +18,53 @@ function toggleburger() {
     let menuBox = document.getElementById('menubox');
     menuBox.classList.toggle('active');
 }
+
+
+document.querySelectorAll('.increase-btn').forEach(button => {
+    button.addEventListener('click', function() {
+        let input = this.parentElement.querySelector('.quantity-input');
+        input.value = parseInt(input.value) + 1;
+    });
+});
+
+document.querySelectorAll('.decrease-btn').forEach(button => {
+    button.addEventListener('click', function() {
+        let input = this.parentElement.querySelector('.quantity-input');
+        if (parseInt(input.value) > parseInt(input.min)) {
+            input.value = parseInt(input.value) - 1;
+        }
+    });
+});
+
+document.querySelectorAll('.add-to-cart-btn').forEach(button => {
+  button.addEventListener('click', async function() {
+      const productName = this.parentElement.querySelector('.text-wrapper-14').innerText;
+
+      try {
+          let response = await fetch('http://localhost:3000/check-stock', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ productName: productName })
+          });
+
+          if (!response.ok) {
+              throw new Error(`HTTP error! status: ${response.status}`);
+          }
+
+          let data = await response.json();
+
+          if (data.available) {
+              // Code to add the item to the cart
+              console.log('Item added to cart');
+          } else {
+              // Code to handle out of stock scenario
+              console.log('Item out of stock');
+          }
+      } catch (error) {
+          console.error('Error:', error);
+      }
+  });
+});
+  
